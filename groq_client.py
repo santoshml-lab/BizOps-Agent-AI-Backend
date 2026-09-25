df73cf4aed90f330c08612961cf8f5b3ba359f30
santoshml-lab <santoshkrsbg36@gmail.com>
2026-09-25 22:37:39 +0530

    Create groq_client.py

---
+++ groq_client.py
@@ -0,0 +1,17 @@
+import os
+
+from groq import Groq
+
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+
+if not GROQ_API_KEY:
+    raise ValueError(
+        "GROQ_API_KEY is not configured."
+    )
+
+
+groq_client = Groq(
+    api_key=GROQ_API_KEY
+)
